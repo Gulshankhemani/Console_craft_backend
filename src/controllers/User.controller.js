4c62a2b9aee54a85135031e3f72f51da4b2bd2d0
Gulshankhemani <gulshankhemani2@gamil.com>
2025-03-09 13:30:28 +0530

    add user profile and watch history routes, including password change and account update features

--- src/controllers/User.controller.js
+++ src/controllers/User.controller.js
@@ -6,6 +6,7 @@ import { ApiResponse } from "../utils/ApiRsponse.js";
 import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
 
+
 const generateAccessAndRefereshTokens = async (userId) => {
   try {
     const user = await User.findById(userId);
